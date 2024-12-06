4fc5d46df0cd82f16833174e4ddfb183edda95a4
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 16:08:54 -0700

    updated post form component

--- src/components/forms/PostForm.vue
+++ src/components/forms/PostForm.vue
@@ -1,9 +1,45 @@
 <script setup>
+import { ref } from 'vue'
+import { postsService } from '../../services/PostsService.js'
+import Pop from '../../utils/Pop.js'
+
+const editable = ref({})
+
+async function createPost() {
+  try {
+    await postsService.createPost(editable.value)
+    editable.value = {}
+  } catch (error) {
+    Pop.error(error)
+  }
+}
 </script>
 
 <template>
-  <div class="post-form">
-  </div>
+  <form @submit.prevent="createPost">
+    <div class="card mb-3">
+      <div class="card-body">
+        <textarea
+          v-model="editable.body"
+          required
+          class="form-control"
+          rows="3"
+          placeholder="What's on your mind?"
+        ></textarea>
+        <input
+          v-model="editable.imgUrl"
+          class="form-control mt-2"
+          type="url"
+          placeholder="Image Url (optional)"
+        >
+        <div class="d-flex = justify-content-end mt-2">
+          <button class="btn btn-primary" type="submit">
+            Post
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <style scoped lang="scss">
