7f9058a68206521b3c79b93467b715d3e27b0df9
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 13:01:17 -0700

    implemented post data model

---
+++ src/models/Post.js
@@ -0,0 +1,12 @@
+export class Post {
+  constructor(data) {
+    this.id = data.id
+    this.body = data.body
+    this.imgUrl = data.imgUrl
+    this.creatorId = data.creatorId
+    this.creator = data.creator
+    this.likes = data.likes || []
+    this.createdAt = new Date(data.createdAt)
+    this.updatedAt = new Date(data.updatedAt)
+  }
+}
