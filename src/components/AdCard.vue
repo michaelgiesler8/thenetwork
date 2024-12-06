831623f3c4c1c0a81add4c86cec797bd2e0fc376
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 15:05:00 -0700

    updated ad card

--- src/components/AdCard.vue
+++ src/components/AdCard.vue
@@ -1,8 +1,18 @@
 <script setup>
+defineProps({
+  ad: {
+    type: Object,
+    required: true
+  }
+})
 </script>
 
 <template>
-  <div class="ad-card">
+  <div class="card">
+    <div class="card-body">
+      <h5>{{ ad.title }}</h5>
+      <img :src="ad.tall" alt="Advertisement" class="img-fluid">
+    </div>
   </div>
 </template>
 
