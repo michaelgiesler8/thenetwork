972b391707a71b28274935159b7a14ef946c0240
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 12:58:37 -0700

    removed codeworks logos

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,15 +3,7 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+  hello
 </template>
 
 <style scoped lang="scss">
