65aa3a978645ae73544171b1d4ac4331f65e09b3
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 13:09:57 -0700

    setup base service files with initial methods

---
+++ src/services/AdsService.js
@@ -0,0 +1,12 @@
+import { AppState } from "../AppState.js"
+import { Ad } from "../models/Ad.js"
+import { api } from "./AxiosService.js"
+
+class AdsService {
+  async getAds() {
+    const res = await api.get('api/ads')
+    AppState.ads = res.data.map(a => new Ad(a))
+  }
+}
+
+export const adsService = new AdsService()
---
+++ src/services/PostsService.js
@@ -0,0 +1,14 @@
+import { AppState } from "../AppState.js"
+import { Post } from "../models/Post.js"
+import { api } from "./AxiosService.js"
+
+class PostsService {
+  async getPosts() {
+    const res = await api.get('api.posts')
+    AppState.posts = res.data.posts.map(p => new Post(p))
+    AppState.nextPage = res.data.older
+    AppState.previousPage = res.data.newer
+  }
+}
+
+export const postsService = new PostsService
