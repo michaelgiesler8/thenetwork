9b806375a2fa4ee7a6905cc3efa6d62d7599eb40
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 15:40:27 -0700

    worked out ad card with responsive layout.

--- src/components/AdCard.vue
+++ src/components/AdCard.vue
@@ -8,13 +8,20 @@ defineProps({
 </script>
 
 <template>
-  <div class="card">
-    <div class="card-body">
-      <h5>{{ ad.title }}</h5>
-      <img :src="ad.tall" alt="Advertisement" class="img-fluid">
+  <a :href="ad.linkURL" target="_blank" class="text-decoration-none">
+    <div class="card elevation-2 mb-3">
+      <img :src="ad.tall" :alt="ad.title" class="ad-image">
+      <div class="card-footer text-center">
+        <h6 class="mb-0 text-dark">{{ ad.title }}</h6>
+      </div>
     </div>
-  </div>
+  </a>
 </template>
 
 <style scoped lang="scss">
+.ad-image {
+  width: 100%;
+  object-fit: cover;
+  aspect-ratio: 3/4;
+}
 </style>
