bdd7691102a4d022ef7626be4c726c10df64ab79
Michael Giesler <michael.giesler1@icloud.com>
2024-12-06 13:26:54 -0700

    modified profile page and homepage to test posts and ads service.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,46 @@
 <script setup>
+import { onMounted } from 'vue';
+import { postsService } from "../services/PostsService.js";
+import { adsService } from "../services/AdsService.js";
+import { AppState } from '../AppState';
+import { computed } from 'vue';
+import PostCard from '../components/PostCard.vue';
+import AdCard from '../components/AdCard.vue';
 
+const posts = computed(() => AppState.posts)
+const ads = computed(() => AppState.ads)
+
+onMounted(async () => {
+  try {
+    await postsService.getPosts()
+    await adsService.getAds()
+    console.log('Posts:', AppState.posts)
+    console.log('Ads:', AppState.ads)
+  } catch (error) {
+    console.error('Error loading data:', error)
+  }
+})
 </script>
 
 <template>
-  hello
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8">
+        <div v-if="posts.length">
+          <p>Total Posts: {{ posts.length }}</p>
+          <pre>{{  posts }}</pre>
+        </div>
+        <p v-else>No posts found.</p>
+      </div>
+      <div class="col-md-4">
+        <div v-if="ads.length">
+          <p>Total Ads: {{ ads.length }}</p>
+          <pre>{{ ads }}</pre>
+        </div>
+        <p v-else>No ads found.</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { profilesService } from '../services/ProfilesService.js';
+import { AppState } from '../AppState.js';
+import { computed } from 'vue';
+
+const route = useRoute()
+const profile = computed(() => AppState.activeProfile)
+
+onMounted(async () => {
+  try {
+    await profilesService.getProfile('6582f5bc24ce93f904f77393')
+    console.log('Profile:', AppState.activeProfile)
+  } catch (error) {
+    console.error('Error loading profile:', error)
+  }
+})
 </script>
 
 <template>
   <div class="container">
-    <h1>Profile</h1>
+    <div v-if="profile">
+      <p>Profile loaded:</p>
+      <pre>{{  profile }}</pre>
+    </div>
+    <p v-else>No profile loaded.</p>
   </div>
 </template>
 
